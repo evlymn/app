b48928a287cfc7c9651742bbebce9a746c36a509
Evelyn Mendes <evlymn@gmail.com>
2021-07-24 17:12:37 -0300

    ajustes firebase

--- projects/principal/src/app/shared/services/firebase/authentication/authentication.service.ts
+++ projects/principal/src/app/shared/services/firebase/authentication/authentication.service.ts
@@ -1,24 +1,28 @@
 import { Injectable, NgZone } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import * as firebase from 'firebase/app';
+import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  currentUser: firebase.User;
   constructor(
     private angularFireAuth: AngularFireAuth,
     private router: Router,
     private ngZone: NgZone
   ) {
     this.onAuthStateChanged();
+    this.getCurrentUser();
+
   }
 
   private onAuthStateChanged() {
-    this.angularFireAuth.auth.onAuthStateChanged(user => {
-        if (user) {
+
+    this.angularFireAuth.onAuthStateChanged(user => {
+      if (user) {
         this.ngZone.run(() => {
           this.router.navigate(['']).catch(reason => console.log(reason));
         });
@@ -34,35 +38,35 @@ export class AuthenticationService {
     return this.angularFireAuth.authState;
   }
 
-  public get currentUser(): firebase.User {
-    return this.angularFireAuth.auth.currentUser;
+  getCurrentUser() {
+    return this.angularFireAuth.currentUser.then(user => this.currentUser = user);
   }
 
   signInWithGoogleAuthProvider(): Promise<firebase.auth.UserCredential> {
-    return this.angularFireAuth.auth.signInWithPopup(
+    return this.angularFireAuth.signInWithPopup(
       new firebase.auth.GoogleAuthProvider()
     );
   }
 
   signInWithFacebookAuthProvider(): Promise<firebase.auth.UserCredential> {
-    return this.angularFireAuth.auth.signInWithPopup(
+    return this.angularFireAuth.signInWithPopup(
       new firebase.auth.FacebookAuthProvider()
     );
   }
 
   signInWithGithubAuthProvider(): Promise<firebase.auth.UserCredential> {
-    return this.angularFireAuth.auth.signInWithPopup(
+    return this.angularFireAuth.signInWithPopup(
       new firebase.auth.GithubAuthProvider()
     );
   }
 
   signInWithTwitterAuthProvider(): Promise<firebase.auth.UserCredential> {
-    return this.angularFireAuth.auth.signInWithPopup(
+    return this.angularFireAuth.signInWithPopup(
       new firebase.auth.TwitterAuthProvider()
     );
   }
 
   signOut() {
-    return this.angularFireAuth.auth.signOut();
+    return this.angularFireAuth.signOut();
   }
 }
